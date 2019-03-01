2be42a8780add8d39471076f23fd8531ef1c16f7
EduPH <epaluzohidalgo@gmail.com>
2019-03-01 17:36:06 +0100

    Update persistent_entropy.py
    minor correction

--- Python/Functions/Persistent entropy/persistent_entropy.py
+++ Python/Functions/Persistent entropy/persistent_entropy.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def persistent_entropy(dgm,dim, inf = False, valInf = np.float(0.0)):
-    # From a diagram generated using ripser, this function compute its persistent 
+    # From a diagram generated using ripser, this function computes its persistent 
     # entropy. If inf = True, this mean that we want to keep infinity bars. Therefore,
     # it is important to give a value to the infinity valInf. To keep stability,
     # the value must be the same for all persistence diagram we are comparing.
